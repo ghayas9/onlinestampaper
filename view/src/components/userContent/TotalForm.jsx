f7a8a44ebc60ba61fceff37343a13be1196ab35d
mmustafa56 <113990354+mmustafa56@users.noreply.github.com>
2023-03-11 16:19:19 +0500

    Delete view directory

--- view/src/components/userContent/TotalForm.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-const TotalForm = () => {
-  return (
-    <>
-    <p>In this page we will render all the verified form from the database.</p>
-    </>
-  )
-}
-export default TotalForm;
